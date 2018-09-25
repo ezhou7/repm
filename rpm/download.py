c509f26c8ecf87ef2830c384da07b3190898f710
Ethan Zhou <ethan.zhou@emory.edu>
2018-09-24 23:23:34 -0700

    outline for create new research project and download research project

---
+++ rpm/download.py
@@ -0,0 +1,20 @@
+def download_research_code_package(package_name, package_version, package_version_set):
+    # TODO: implement this method
+    # search for research code
+    # if found, download package, else return error message and closest matches
+    pass
+
+
+def download_research_dataset(dataset_name, dataset_version):
+    # TODO: implement this method
+    # search for research dataset
+    # if found, download dataset, else return error message and closest matches
+    pass
+
+
+def download_research_project(project_name, project_version, project_version_set):
+    # TODO: implement this method
+    # search for research project
+    # if found, download project code package and dataset, else return error message and closest matches
+    # ask if user wishes to download code, dataset or both code and dataset
+    pass
---
+++ rpm/project.py
@@ -0,0 +1,52 @@
+def create_new_research_project():
+    # TODO: implement this method
+    # ask user to set a default directory (optional)
+    # ask user to create/set a project directory
+    # set up project directory with git and .gitignore
+    # set up project directory with setup.py
+    # set up project directory with data directory
+    # set up project directory with config/props directory
+    # set up project directory with resources directory
+    pass
+
+
+def create_default_directory():
+    # TODO: implement this method
+    # ask user to set a default directory (optional)
+    pass
+
+
+def create_project_directory():
+    # TODO: implement this method
+    # ask user to create a project directory
+    pass
+
+
+def init_project_directory_with_git():
+    # TODO: implement this method
+    # initialize the new project directory with git
+    pass
+
+
+def init_project_directory_with_setup_py():
+    # TODO: implement this method
+    # initialize the new project directory with setup.py
+    pass
+
+
+def init_project_directory_with_data_sub_directory():
+    # TODO: implement this method
+    # initialize the new project directory with a data sub-directory
+    pass
+
+
+def init_project_directory_with_config_sub_directory():
+    # TODO: implement this method
+    # initialize the new project directory with a config sub-directory
+    pass
+
+
+def init_project_directory_with_resources_sub_directory():
+    # TODO: implement this method
+    # initialize the new project directory with a resources sub-directory
+    pass
