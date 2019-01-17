d4e91535f0eb97eeba870412c6d53a12d6da5c1d
E <>
2019-01-16 22:48:55 -0800

    auto-setup git and setup.py in user created project

--- rpm/project.py
+++ rpm/project.py
@@ -21,6 +21,7 @@ def create_default_directory():
 
 
 def create_project_directory(project_name):
+    # initialize the new project directory
     if not os.path.exists(project_name):
         print("Creating project {}...".format(project_name))
         os.mkdir(project_name)
@@ -28,17 +29,34 @@ def create_project_directory(project_name):
     else:
         print("Cannot create project {0}; directory {0} already exists".format(project_name))
 
+    os.chdir(project_name)
+
 
 def init_project_directory_with_git():
-    # TODO: implement this method
     # initialize the new project directory with git
-    pass
+    git.init()
 
 
-def init_project_directory_with_setup_py():
-    # TODO: implement this method
+def init_project_directory_with_setup_py(project_name):
     # initialize the new project directory with setup.py
-    pass
+    with open("setup.py", "w") as fin:
+        fin.write("from setuptools import setup, find_packages\n")
+        fin.write("\n\n")
+
+        fin.write("def setup_package:\n")
+        fin.write("\tmetadata = dict(\n")
+        fin.write("\t\tname=\"{}\",\n".format(project_name))
+        fin.write("\t\tversion=\"1.0.0\",\n")
+        fin.write("\t\tdescription=\"{}\",\n".format(project_name))
+        fin.write("\t\tpackages=\"find_packages()\",\n")
+        fin.write("\t\tinstall_requires=[]\n")
+        fin.write(")\n")
+        fin.write("\n")
+        fin.write("setup(**metadata)")
+        fin.write("\n\n")
+
+        fin.write("if __name__ == \"__main__\":\n")
+        fin.write("\tsetup_package()\n")
 
 
 def init_project_directory_with_data_sub_directory():
