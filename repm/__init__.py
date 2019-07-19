706f16b4a4b62ec6987d5ee3b64e193a168643c6
E <>
2019-07-18 23:44:03 -0700

    add server api url

--- repm/__init__.py
+++ repm/__init__.py
@@ -4,3 +4,11 @@ import os
 CODE_PACKAGE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 RESOURCES_ROOT_DIR = os.path.join(CODE_PACKAGE_ROOT_DIR, "resources")
+
+SERVER_API_URL = "https://repm.io/api"
+
+
+def get_api_url(api_path: str):
+    return SERVER_API_URL + api_path \
+        if api_path.startswith("/") \
+        else "{}/{}".format(SERVER_API_URL, api_path)
