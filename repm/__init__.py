53d03258c63780f3cc6b0b77717c180f933fc517
E <>
2019-07-21 19:16:59 -0700

    auth functionality

--- repm/__init__.py
+++ repm/__init__.py
@@ -1,9 +1,20 @@
 import os
+from pathlib import Path
 
 
 CODE_PACKAGE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 RESOURCES_ROOT_DIR = os.path.join(CODE_PACKAGE_ROOT_DIR, "resources")
+
+GLOBAL_REPM_DIR = os.path.join(str(Path.home()), "/.repm")
+
+
+def get_global_file_path(relative_file_path: str):
+    if not os.path.exists(GLOBAL_REPM_DIR):
+        os.mkdir(GLOBAL_REPM_DIR)
+
+    return os.path.join(GLOBAL_REPM_DIR, relative_file_path)
+
 
 SERVER_API_URL = "https://repm.io/api"
 
---
+++ repm/auth.py
@@ -0,0 +1,59 @@
+import os
+import pickle
+import requests
+from typing import Union
+
+from repm import get_api_url, get_global_file_path
+
+
+def __get_active_session() -> Union[requests.Session, None]:
+    active_session_file_path = get_global_file_path("/active_session.pkl")
+    if not os.path.exists(active_session_file_path):
+        return None
+
+    with open(active_session_file_path, "r") as fin:
+        return pickle.load(fin)
+
+
+def __set_active_session(active_session: Union[requests.Session, None]):
+    active_session_file_path = get_global_file_path("/active_session.pkl")
+    with open(active_session_file_path, "w") as fout:
+        if active_session:
+            pickle.dump(active_session, fout)
+        else:
+            fout.write("")
+
+
+def login(email: str, password: str):
+    active_session = __get_active_session()
+    if active_session:
+        print("Already logged in.")
+        return
+
+    session = requests.Session()
+    login_url = get_api_url("/auth/login")
+    login_details = {
+        "email": email,
+        "password": password
+    }
+    login_res = session.post(login_url, data=login_details)
+
+    if login_res.status_code == 200:
+        __set_active_session(session)
+        print("Successfully logged in.")
+    elif login_res.status_code == 401:
+        print(login_res.content)
+
+
+def logout():
+    active_session = __get_active_session()
+    if not active_session:
+        print("Already logged out.")
+        return
+
+    logout_url = get_api_url("/auth/logout")
+    logout_res = active_session.get(logout_url, cookies=active_session.cookies.get_dict())
+
+    if logout_res.status_code == 200:
+        __set_active_session(None)
+        print("Successfully logged out.")
