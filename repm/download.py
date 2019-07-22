0afe1b34727f9ee6ed15fd42d4eaa64ec9a3c3a5
E <>
2019-07-21 18:54:07 -0700

    download functionality

--- repm/download.py
+++ repm/download.py
@@ -1,22 +1,39 @@
 import os
-import re
-from google.cloud import storage
+import zipfile
+import requests
 
+from repm import get_api_url
 from repm.dirutils import get_repm_project_root_directory
 
 
-def download_research_dataset(dataset_name: str):
-    storage_client = storage.Client.create_anonymous_client()
-    bucket = storage_client.bucket(dataset_name)
+def download_dataset(dataset_name: str):
+    download_url = get_api_url("/datasets")
 
     project_root_path = get_repm_project_root_directory()
     if project_root_path:
         download_path = "{}/data/{}/".format(project_root_path, dataset_name)
-        if not os.path.exists(download_path):
-            os.mkdir(download_path)
     else:
         raise Exception("Not within a repm project")
 
-    for blob in bucket.list_blobs():
-        file_path = os.path.join(download_path, os.path.basename(blob.name))
-        bucket.blob(blob.name).download_to_filename(file_path)
+    return __download_zip(dataset_name, download_url, download_path)
+
+
+def __download_zip(dataset_name: str, url: str, download_path: str):
+    with requests.get(url, params={"bucketName": dataset_name}, stream=True) as response:
+        response.raise_for_status()
+        zip_path = download_path[:-1] + ".zip"
+
+        with open(zip_path, "wb") as fout:
+            for chunk in response.iter_content(chunk_size=8192):
+                # filter out keep-alive new chunks
+                if chunk:
+                    fout.write(chunk)
+            fout.flush()
+
+    fzip = zipfile.ZipFile(zip_path, "r")
+    fzip.extractall(download_path)
+    fzip.close()
+
+    os.remove(zip_path)
+
+    return download_path
