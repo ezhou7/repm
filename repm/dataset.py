e017024f6428a08485d978e5254b74d1ec72bfb5
E <>
2019-08-18 10:36:36 -0700

    list datasets functionality

---
+++ repm/dataset.py
@@ -0,0 +1,16 @@
+import requests
+
+from repm import get_api_url
+
+
+def list_datasets():
+    list_datasets_url: str = get_api_url("/dataset/list")
+    list_datasets_res = requests.get(list_datasets_url)
+
+    if list_datasets_res.status_code == 200:
+        dataset_names = list_datasets_res.json()
+
+        for name in dataset_names:
+            print(name)
+    else:
+        print(list_datasets_res.content)
--- repm/main.py
+++ repm/main.py
@@ -1,13 +1,14 @@
 import os
-import argparse
+from argparse import ArgumentParser, Action
 from repm.project import create_new_research_project
 from repm.download import download_dataset
 from repm.upload import upload_dataset
 from repm.auth import login, logout, signup
+from repm.dataset import list_datasets
 
 
 def main():
-    arg_parser = argparse.ArgumentParser()
+    arg_parser = ArgumentParser()
 
     # main arguments
     arg_parser.add_argument("-i", "--init", help="Initialize current directory as a new research project",
@@ -15,6 +16,7 @@ def main():
     arg_parser.add_argument("-l", "--login", help="Login with a username and password", nargs=0, action=LoginAction)
     arg_parser.add_argument("-o", "--logout", help="Logout of a session", nargs=0, action=LogoutAction)
     arg_parser.add_argument("-s", "--signup", help="Signup with a username and password", nargs=0, action=SignupAction)
+    arg_parser.add_argument("-t", "--list", help="List all publich datasets", nargs=0, action=ListDatasetsAction)
 
     sub_parsers = arg_parser.add_subparsers()
 
@@ -27,7 +29,7 @@ def main():
     arg_parser.parse_args()
 
 
-class InitAction(argparse.Action):
+class InitAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         cwd = os.getcwd()
         project_path = "{}/{}".format(cwd, values) if values is not None else cwd
@@ -38,22 +40,27 @@ class InitAction(argparse.Action):
         create_new_research_project(values[0])
 
 
-class LoginAction(argparse.Action):
+class LoginAction(Action):
     def __call__(self, *args, **kwargs):
         login()
 
 
-class LogoutAction(argparse.Action):
+class LogoutAction(Action):
     def __call__(self, *args, **kwargs):
         logout()
 
 
-class SignupAction(argparse.Action):
+class SignupAction(Action):
     def __call__(self, *args, **kwargs):
         signup()
 
 
-class DownloadDatasetAction(argparse.Action):
+class ListDatasetsAction(Action):
+    def __call__(self, *args, **kwargs):
+        list_datasets()
+
+
+class DownloadDatasetAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         if values is None or len(values) < 1:
             raise Exception("Must provide the name of an existing project.")
@@ -61,7 +68,7 @@ class DownloadDatasetAction(argparse.Action):
         download_dataset(values[0])
 
 
-class UploadDatasetAction(argparse.Action):
+class UploadDatasetAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         if values is None or len(values) < 1:
             raise Exception("Must provide the name of an existing project.")
